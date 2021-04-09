99ee65690924edc7b344daaf1f5a6e643813710a
NeaMarySm <neamarysm@gmail.com>
2021-04-09 12:21:31 +0300

    task_1 version2

--- lesson4/1.js
+++ lesson4/1.js
@@ -1,24 +1,48 @@
 'use strict';
 
-let obj = {
+// let obj = {
+//     сотни: null,
+//     десятки: null,
+//     еденицы: null,
+//     numberAdd(){
+
+//         while(true) {
+//             let num = +prompt ('Введите целое число от 0 до 999 или -1 для выхода');
+//             if (num === -1) break;
+//             if (!Number.isInteger(num) || num < 0 || num > 999){
+//                 alert('Число не является целым, либо не находится в диапазоне от 0 до 999');
+//                 continue;
+//             } 
+//             this.еденицы = num % 10;
+//             this.десятки = Math.floor(num / 10) % 10;
+//             this.сотни = Math.floor(num / 100);
+//             return;
+//         }       
+//     }
+// }
+// obj.numberAdd();
+// console.log(obj);
+
+let obj1 = {
     сотни: null,
     десятки: null,
-    еденицы: null,
-    numberAdd(){
-
-        while(true) {
-            let num = +prompt ('Введите целое число от 0 до 999 или -1 для выхода');
-            if (num === -1) break;
-            if (!Number.isInteger(num) || num < 0 || num > 999){
-                alert('Число не является целым, либо не находится в диапазоне от 0 до 999');
-                continue;
-            } 
-            this.еденицы = num % 10;
-            this.десятки = Math.floor(num / 10) % 10;
-            this.сотни = Math.floor(num / 100);
-            return;
-        }       
-    }
+    еденицы: null, 
 }
-obj.numberAdd();
-console.log(obj);
+
+function  numberAdd(obj){
+
+    while(true) {
+        let num = +prompt ('Введите целое число от 0 до 999 или -1 для выхода');
+        if (num === -1) break;
+        if (!Number.isInteger(num) || num < 0 || num > 999){
+            alert('Число не является целым, либо не находится в диапазоне от 0 до 999');
+            continue;
+        } 
+        obj.еденицы = num % 10;
+        obj.десятки = Math.floor(num / 10) % 10;
+        obj.сотни = Math.floor(num / 100);
+        return;
+    }       
+}
+numberAdd(obj1);
+console.log(obj1);
